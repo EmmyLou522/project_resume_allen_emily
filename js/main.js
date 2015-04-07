59ff972071e6d5d066afb66acea02905c07aa990
Emily Allen <emmylou522@gmail.com>
2015-04-06 20:45:03 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    paragraphs = $( "p" ).css({
+        "background-color": "black",
+        "color": "white",
+        "font-size": "2em"
+    });
+});
+
+//* Don't you love my excellent style choices? hehe*//
