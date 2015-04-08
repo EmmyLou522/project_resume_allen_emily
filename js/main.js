901e85fd4aadff615e907b92836bf1f4f78af425
Emily Allen <emmylou522@gmail.com>
2015-04-07 21:32:22 -0400

    style fonts and image for mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
-$(document).ready(function () {
-    paragraphs = $( "p" ).css({
-        "background-color": "black",
-        "color": "white",
-        "font-size": "2em"
-    });
-});
 
 //* Don't you love my excellent style choices? hehe*//
